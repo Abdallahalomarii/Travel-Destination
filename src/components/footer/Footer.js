e48cd464a9486f6da0012c02650da1592f8f75cc
Abboodo <abboodoa@gmail.com>
2023-04-19 00:52:33 +0000

    class-11 branch completed

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,18 @@
+import './Footer.css';
+function Footer() {
+    return (
+        
+
+        <div className='Footer'>
+            <div className="social">
+            <a href="#"><i className="fa-brands fa-instagram"></i></a>
+            <a href="#"><i className="fa-brands fa-facebook"></i></a>
+            <a href="#"><i className="fa-brands fa-twitter"></i></a>
+        </div>
+            <div className="Author">
+                Abdallah alomari
+            </div>
+        </div>
+    )
+}
+export default Footer;
---
+++ src/components/header/Header.js
@@ -0,0 +1,10 @@
+import './Header.css';
+function Header ()
+{
+    return (<div className='header'>
+        <h1>
+        Travel Destination
+        </h1>
+    </div>)
+}
+export default Header
